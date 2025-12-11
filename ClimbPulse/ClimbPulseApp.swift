300eded0c9737aa312df2b6523b35356687eb938
Mansour Taleshi <mansour.taleshi@gmail.com>
2025-12-11 16:44:05 +0200

    Initial Commit

---
+++ ClimbPulse/ClimbPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbPulseApp.swift
+//  ClimbPulse
+//
+//  Created by Mansour Taleshi on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClimbPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimbPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClimbPulse
+//
+//  Created by Mansour Taleshi on 11.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
